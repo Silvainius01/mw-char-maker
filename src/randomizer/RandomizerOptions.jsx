bcaa4b6d5ea9a192a085d71dc7444eae647ec4dc
Silvainius <connor.adam@ymail.com>
2026-06-10 05:37:22 -0700

    Fixed option elements being initialized incorrectly

--- src/randomizer/RandomizerOptions.jsx
+++ src/randomizer/RandomizerOptions.jsx
@@ -5,20 +5,23 @@ import RandomizerOptionElement from "./RandomizerOptionElement";
 function RandomizerOptions(props) {
     const hookDict = {}
     const settings = props.randomizer.settings;
+    const capFirst = (s) => { return s.replace(/^./, s[0].toUpperCase()); }
+    const initNumber = (key) => { return settings[key].default.toString(); }
+    const initButton = (key) => { return capFirst(settings[key].default.toString()); }
 
     // Numeric options
-    const [n0, setN0] = useState(settings['NumAttributes'].value); hookDict['NumAttributes'] = { value: n0, setter: setN0 };
-    const [n1, setN1] = useState(settings['MaxWeaponSkills'].value); hookDict['MaxWeaponSkills'] = { value: n1, setter: setN1 };
-    const [n2, setN2] = useState(settings['MaxArmorSkills'].value); hookDict['MaxArmorSkills'] = { value: n2, setter: setN2 };
-    const [n3, setN3] = useState(settings['MaxLockSkills'].value); hookDict['MaxLockSkills'] = { value: n3, setter: setN3 };
-    const [n4, setN4] = useState(settings['RaceSkillWeight'].value); hookDict['RaceSkillWeight'] = { value: n4, setter: setN4 };
-    const [n5, setN5] = useState(settings['RaceAttributeWeight'].value); hookDict['RaceAttributeWeight'] = { value: n5, setter: setN5 };
-    const [n6, setN6] = useState(settings['MinRaceScore'].value); hookDict['MinRaceScore'] = { value: n6, setter: setN6 };
-    const [n7, setN7] = useState(settings['FactionSkillWeight'].value); hookDict['FactionSkillWeight'] = { value: n7, setter: setN7 };
-    const [n8, setN8] = useState(settings['FactionAttributeWeight'].value); hookDict['FactionAttributeWeight'] = { value: n8, setter: setN8 };
-    const [n9, setN9] = useState(settings['MinFactionScore'].value); hookDict['MinFactionScore'] = { value: n9, setter: setN9 };
-    const [n10, setN10] = useState(settings['MajorSkillSpecWeight'].value); hookDict['MajorSkillSpecWeight'] = { value: n10, setter: setN10 };
-    const [n11, setN11] = useState(settings['MinorSkillSpecWeight'].value); hookDict['MinorSkillSpecWeight'] = { value: n11, setter: setN11 };
+    const [n0, setN0] = useState(initNumber('NumAttributes')); hookDict['NumAttributes'] = { value: n0, setter: setN0 };
+    const [n1, setN1] = useState(initNumber('MaxWeaponSkills')); hookDict['MaxWeaponSkills'] = { value: n1, setter: setN1 };
+    const [n2, setN2] = useState(initNumber('MaxArmorSkills')); hookDict['MaxArmorSkills'] = { value: n2, setter: setN2 };
+    const [n3, setN3] = useState(initNumber('MaxLockSkills')); hookDict['MaxLockSkills'] = { value: n3, setter: setN3 };
+    const [n4, setN4] = useState(initNumber('RaceSkillWeight')); hookDict['RaceSkillWeight'] = { value: n4, setter: setN4 };
+    const [n5, setN5] = useState(initNumber('RaceAttributeWeight')); hookDict['RaceAttributeWeight'] = { value: n5, setter: setN5 };
+    const [n6, setN6] = useState(initNumber('MinRaceScore')); hookDict['MinRaceScore'] = { value: n6, setter: setN6 };
+    const [n7, setN7] = useState(initNumber('FactionSkillWeight')); hookDict['FactionSkillWeight'] = { value: n7, setter: setN7 };
+    const [n8, setN8] = useState(initNumber('FactionAttributeWeight')); hookDict['FactionAttributeWeight'] = { value: n8, setter: setN8 };
+    const [n9, setN9] = useState(initNumber('MinFactionScore')); hookDict['MinFactionScore'] = { value: n9, setter: setN9 };
+    const [n10, setN10] = useState(initNumber('MajorSkillSpecWeight')); hookDict['MajorSkillSpecWeight'] = { value: n10, setter: setN10 };
+    const [n11, setN11] = useState(initNumber('MinorSkillSpecWeight')); hookDict['MinorSkillSpecWeight'] = { value: n11, setter: setN11 };
     let numberUpdate = (key, cv) => {
         let nv = Number(cv);
 
@@ -31,20 +34,21 @@ function RandomizerOptions(props) {
     }
 
     // Toggle options
-    const [b0, setB0] = useState(settings['AllowAdvFactionPairs'].value); hookDict['AllowAdvFactionPairs'] = { value: b0, setter: setB0 };
-    const [b1, setB1] = useState(settings['ForceLockSkill'].value); hookDict['ForceLockSkill'] = { value: b1, setter: setB1 };
-    const [b2, setB2] = useState(settings['ForceWeaponSkill'].value); hookDict['ForceWeaponSkill'] = { value: b2, setter: setB2 };
-    const [b3, setB3] = useState(settings['ForceArmorSkill'].value); hookDict['ForceArmorSkill'] = { value: b3, setter: setB3 };
-    const [b4, setB4] = useState(settings['ForceTrainableAttrs'].value); hookDict['ForceTrainableAttrs'] = { value: b4, setter: setB4 };
+    const [b0, setB0] = useState(initButton('AllowAdvFactionPairs')); hookDict['AllowAdvFactionPairs'] = { value: b0, setter: setB0 };
+    const [b1, setB1] = useState(initButton('ForceLockSkill')); hookDict['ForceLockSkill'] = { value: b1, setter: setB1 };
+    const [b2, setB2] = useState(initButton('ForceWeaponSkill')); hookDict['ForceWeaponSkill'] = { value: b2, setter: setB2 };
+    const [b3, setB3] = useState(initButton('ForceArmorSkill')); hookDict['ForceArmorSkill'] = { value: b3, setter: setB3 };
+    const [b4, setB4] = useState(initButton('ForceTrainableAttrs')); hookDict['ForceTrainableAttrs'] = { value: b4, setter: setB4 };
     let buttonUpdate = (key, cv) => {
+        // Button value comes in as a string
         const nv = cv === "false" ? true : false;
 
         // Capitalize first letter
-        let str = nv.toString();
-        str = str.replace(/^./, str[0].toUpperCase());
+        // let str = nv.toString();
+        // str = str.replace(/^./, str[0].toUpperCase());
 
         settings[key].value = nv;
-        hookDict[key].setter(str);
+        hookDict[key].setter(capFirst(nv.toString()));
         console.log(settings[key].name + " -> " + nv);
     }
 
